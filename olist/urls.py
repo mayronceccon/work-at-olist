fdc21e726b2bf1528bf4443c2c9dddb68634d72c
Mayron Ceccon <mayron.ceccon@tecnorisk.com.br>
2018-03-12 14:32:44 -0300

    to change documentation for Swagger

--- olist/urls.py
+++ olist/urls.py
@@ -1,16 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url
-from rest_framework.documentation import include_docs_urls
+from rest_framework_swagger.views import get_swagger_view
 
 from prices.views import PricesView
 
+schema_view = get_swagger_view(title='Work To Olist')
+
 urlpatterns = [
+    url(r'^docs/$', schema_view),
     path('admin/', admin.site.urls),
-    url(r'^docs/', include_docs_urls(
-                title='Work To Olist',
-                description='Documentation of project Work To Olist'
-            )
-        ),
     url(r'api/v1/prices', PricesView.as_view())
 ]
