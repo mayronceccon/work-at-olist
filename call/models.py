0b73ab74ab31eca269014f9ac2041544924bfee4
Mayron Ceccon <mayron.ceccon@gmail.com>
2018-03-12 19:59:36 -0300

    create call model

--- call/models.py
+++ call/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Call(models.Model):
+    TYPES_CALL = (
+        ('start', 'Start'),
+        ('end', 'End'),
+    )
+    type = models.CharField(
+        max_length=10,
+        choices=TYPES_CALL
+    )
+    start = models.DateTimeField()
+    end = models.DateTimeField(blank=True, null=True)
+    call_id_external = models.CharField(max_length=255)
+    source = models.CharField(max_length=11)
+    destination = models.CharField(max_length=11)
+
+    def __str__(self):
+        return "%s to %s" % (self.source, self.destination)
