bd4482e59a1e2912780df7ffc05979d1e93c7f53
Mayron Ceccon <mayron.ceccon@gmail.com>
2018-03-11 22:43:58 -0300

    add initial documentation

--- olist/urls.py
+++ olist/urls.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url
+from rest_framework.documentation import include_docs_urls
 
 from prices.views import PricesView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^docs/', include_docs_urls(
+                title='Work To Olist',
+                description='Documentation of project Work To Olist'
+            )
+        ),
     url(r'api/v1/prices', PricesView.as_view())
 ]
--- prices/views.py
+++ prices/views.py
@@ -3,5 +3,9 @@ from rest_framework.response import Response
 
 
 class PricesView(APIView):
+    """
+    get:
+    Return a list of all the existing prices.
+    """
     def get(self, request):
         return Response({'test': 'It worked!'})
